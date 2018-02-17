b640561fd58d1501778126304b418406d7281c5f
Daniel Vinntreus <gudski@gmail.com>
2018-02-17 15:03:46 +0100

    Write message

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,37 @@ class Home extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      message: ''
+      message: '',
+      messages: []
     };
     this.onMessageChange = this.onMessageChange.bind(this);
+    this.onMessageKeyPress = this.onMessageKeyPress.bind(this);
   }
   onMessageChange(event){
     this.setState({message: event.target.value});
   }
+  onMessageKeyPress(event){
+    if(event.keyCode === 13 && this.state.message){
+      let messages = this.state.messages;
+      messages.push({text: this.state.message});
+      this.setState({messages: messages, message: ''});
+    }
+  }
   render() {
+    const messages = this.state.messages.map((m, i) => {
+      return (
+        <div key={i} className='Message-item'>{m.text}</div>
+      );
+    });
     return (
       <div className='Home'>
         <div className='Home-sidebar'>
             <h2>Channel name</h2>
         </div>
-        <div className='Home-header'>
-
+        <div className='Home-header'></div>
+        <div className='Home-main'>
+          <div className='Message-list'>{messages}</div>
         </div>
-        <div className='Home-main'></div>
         <div className='Home-footer'>
             <input
               className='Message-input'
@@ -29,6 +43,7 @@ class Home extends React.Component {
               placeholder='Write something'
               value={this.state.message}
               onChange={this.onMessageChange}
+              onKeyDown={this.onMessageKeyPress}
             />
         </div>
       </div>
--- src/Home.test.js
+++ src/Home.test.js
@@ -18,5 +18,17 @@ describe('Home', () => {
       ReactTestUtils.Simulate.change(input);
       expect(c.state.message).toBe('b');
     });
+    it('display messages that are sent and clear input', () => {
+      const div = document.createElement('div');
+      let c = ReactDOM.render(<Home />, div);
+      c.setState({message: 'a'});
+      let messages = div.getElementsByClassName('Message-list')[0].children;
+      expect(messages.length).toBe(0);
+      let input = div.getElementsByClassName('Message-input')[0];
+      ReactTestUtils.Simulate.keyDown(input, {key: 'Enter', keyCode: 13, which: 13});
+      expect(messages.length).toBe(1);
+      expect(messages[0].textContent).toBe('a');
+      expect(c.state.message).toBe('');
+    });
   });
 });
