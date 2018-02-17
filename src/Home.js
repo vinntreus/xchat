b809bc6967e169883e13dd4c58a9bb23dd4f98d2
Daniel Vinntreus <gudski@gmail.com>
2018-02-17 15:48:34 +0100

    Indicate messages is sending

--- src/Home.js
+++ src/Home.js
@@ -11,20 +11,38 @@ class Home extends React.Component {
     this.onMessageChange = this.onMessageChange.bind(this);
     this.onMessageKeyPress = this.onMessageKeyPress.bind(this);
   }
+  sendMessage(message){
+    let self = this;
+    setTimeout(() => self.messageSent(message), 1000);
+  }
+  messageSent(message){
+    let messages = this.state.messages;
+    const index = messages.indexOf(message);
+    if(index !== -1){
+      messages[index].state = '';
+      this.setState({messages: messages});
+    }
+  }
   onMessageChange(event){
     this.setState({message: event.target.value});
   }
   onMessageKeyPress(event){
     if(event.keyCode === 13 && this.state.message){
       let messages = this.state.messages;
-      messages.push({text: this.state.message});
+      let message = {
+        text: this.state.message,
+        state: 'sending'
+      };
+      messages.push(message);
       this.setState({messages: messages, message: ''});
+      this.sendMessage(message);
     }
   }
   render() {
     const messages = this.state.messages.map((m, i) => {
+      let css = ['Message-item', m.state].join(' ');
       return (
-        <div key={i} className='Message-item'>{m.text}</div>
+        <div key={i} className={css}>{m.text}</div>
       );
     });
     return (
--- src/Home.test.js
+++ src/Home.test.js
@@ -21,14 +21,21 @@ describe('Home', () => {
     it('display messages that are sent and clear input', () => {
       const div = document.createElement('div');
       let c = ReactDOM.render(<Home />, div);
+      c.sendMessage = (m) => 1; // mock away actual sending
       c.setState({message: 'a'});
       let messages = div.getElementsByClassName('Message-list')[0].children;
       expect(messages.length).toBe(0);
       let input = div.getElementsByClassName('Message-input')[0];
       ReactTestUtils.Simulate.keyDown(input, {key: 'Enter', keyCode: 13, which: 13});
+      // one message is output with highlight of sending
       expect(messages.length).toBe(1);
       expect(messages[0].textContent).toBe('a');
+      expect(messages[0].classList).toContain('sending');
+      // clears input
       expect(c.state.message).toBe('');
+      // simulate message was successfully sent
+      c.messageSent(c.state.messages[0]);
+      expect(messages[0].classList).not.toContain('sending');
     });
   });
 });
